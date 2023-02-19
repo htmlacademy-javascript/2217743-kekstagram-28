fecd6beaa10053c4e77d1be8cef2c8a784f2f533
Temmatema <112764243+Temmatema@users.noreply.github.com>
2023-02-19 10:43:55 +0300

    исправятся ошибки

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,26 @@
-function stringLength(str) {
-  return str.length > 10
-}
+// eslint-disable-next-line no-unused-vars
+const stringLength = (str, number) => str.length >= number;
 
-function palindromeCheck(str) {
-  let newStr = str.toLowerCase().split('').reverse()
-    .filter(el => el !== ' ').join('');
+// eslint-disable-next-line no-unused-vars
+const palindromeCheck = (str) => {
+  const newStr = str.toLowerCase().split('').reverse()
+    .filter((el) => el !== ' ').join('');
   return newStr === str.trim().toLowerCase().split(' ').join('');
-}
+};
 
-function findNumbers(value) {
+// eslint-disable-next-line no-unused-vars
+const findNumbers = (value) => {
   if (!value) {
     return NaN;
-  } else if (typeof value != 'number') {
-    value = value.split(' ').filter(el => {
-      return !isNaN(el);
-    }).join('');
-    value = value.length > 0 ? parseInt(value) : NaN;
+  } else if (typeof value !== 'number') {
+    value = value.split(' ').filter((el) => !isNaN(el)).join('').replace(/[\s.,%]/g, '');
+    value = value.length > 0 ? parseInt(value, 10) : NaN;
   }
   return value;
-}
+};
 
-function completeString(str, strLength, addSymbol) {
+// eslint-disable-next-line no-unused-vars
+const completeString = (str, strLength, addSymbol) => {
   if (str.length < strLength) {
     addSymbol = addSymbol.split('');
     str = str.split('');
@@ -32,12 +32,14 @@ function completeString(str, strLength, addSymbol) {
         str.unshift(addSymbol[addSymbol.length - i]);
       }
     }
-    str = str.map(el => {
-      if (typeof el == 'undefined') {
+    str = str.map((el) => {
+      if (typeof el === 'undefined') {
         el = addSymbol[0];
       }
       return el;
-    })
+    });
   }
   return str.join('');
-}
+};
+
+
