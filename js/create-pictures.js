eded3e459a00c969e7a355367f7320623ca559d0
Temmatema <112764243+Temmatema@users.noreply.github.com>
2023-02-28 12:39:11 +0300

    eslint fix

--- js/create-pictures.js
+++ js/create-pictures.js
@@ -1,16 +1,16 @@
 import {similarObjects} from './data.js';
 
-let imgUpload = document.querySelector('.img-upload');
-let picture = document.querySelector('#picture').content.querySelector('.picture');
+const imgUpload = document.querySelector('.img-upload');
+const picture = document.querySelector('#picture').content.querySelector('.picture');
 
 const similarPictures = document.createDocumentFragment();
 
 similarObjects.forEach((el) => {
-  let pictureClone = picture.cloneNode(true);
+  const pictureClone = picture.cloneNode(true);
   pictureClone.querySelector('.picture__img').src = el.url;
   pictureClone.querySelector('.picture__comments').textContent = el.comments.length;
   pictureClone.querySelector('.picture__likes').textContent = el.likes;
   similarPictures.appendChild(pictureClone);
-})
+});
 
 imgUpload.appendChild(similarPictures);
