5030923e371b97f54584c2cb9557e7d288a24f34
Temmatema <112764243+Temmatema@users.noreply.github.com>
2023-03-20 11:06:30 +0300

    исправление ошибок

--- js/create-pictures.js
+++ js/create-pictures.js
@@ -1,11 +1,12 @@
 import {similarObjects} from './api.js';
+import {bodyNode, createBigPicture, socialCommentsNode} from './big-picture.js';
 
 const picturesContainerNode = document.querySelector('.pictures');
 const pictureNode = document.querySelector('#picture').content.querySelector('.picture');
 const similarPicturesNode = document.createDocumentFragment();
 export const createPictures = (array) => {
-  const pictures = document.querySelectorAll('.picture');
-  pictures.forEach((pic) => {
+  const picturesNode = document.querySelectorAll('.picture');
+  picturesNode.forEach((pic) => {
     picturesContainerNode.removeChild(pic);
   });
 
@@ -14,6 +15,13 @@ export const createPictures = (array) => {
     pictureClone.querySelector('.picture__img').src = el.url;
     pictureClone.querySelector('.picture__comments').textContent = el.comments.length;
     pictureClone.querySelector('.picture__likes').textContent = el.likes;
+    pictureClone
+      .addEventListener('click', (e) => {
+        e.preventDefault();
+        bodyNode.classList.add('modal-open');
+        socialCommentsNode.innerHTML = '';
+        createBigPicture(el);
+      });
     similarPicturesNode.appendChild(pictureClone);
   });
   return picturesContainerNode.appendChild(similarPicturesNode);
